1e0536aa6a5b4738293fd9c994e0530a5af222d0
Blake Ramsdell <blaker@gmail.com>
2023-11-01 00:20:34 -0700

    Add fast scrolling

--- DerpiCalc/ui.c
+++ DerpiCalc/ui.c
@@ -7,8 +7,13 @@
 
 #include "ui.h"
 
-#define NORMAL_COLOR MAKECOLOR(COLOR_GREEN, COLOR_BLACK)
-#define INVERSE_COLOR MAKECOLOR(COLOR_BLACK, COLOR_GREEN)
+#define CLAMP(X, MAX) ((X) >= (MAX) ? ((X) - (MAX)) : (X))
+#define CLAMP_ADD(X, Y, MAX) ((((MAX) - (X)) <= (Y)) ? ((Y) - ((MAX) - (X))): ((X) + (Y)))
+#define CLAMP_SUB(X, Y, MAX) (((X) < (Y)) ? ((MAX) - ((Y) - (X))) : ((X) - (Y)))
+
+#define NORMAL_COLOR MAKECOLOR(COLOR_GREEN, COLOR_GRAY1)
+#define INVERSE_COLOR MAKECOLOR(COLOR_GRAY1, COLOR_GREEN)
+#define TRANSPARENT_COLOR MAKECOLOR(COLOR_BLACK, COLOR_BLACK)
 
 #define LAYER_UI 1
 #define LAYER_CELLS 0
@@ -20,10 +25,16 @@
 
 #define COLUMN_WIDTH 9
 #define NUMBER_CELL_COLUMNS ((WIDTH_CHARS - 3) / COLUMN_WIDTH)
+#define NUMBER_CELL_ROWS (HEIGHT_CHARS - 4)
+
+#define CANVAS_WIDTH_CHARS 128
+#define CANVAS_HEIGHT_CHARS 64
 
 static cell_ctx g_cell_ctx;
 static uint8_t active_cell_column, active_cell_row;
 static uint8_t ul_cell_column, ul_cell_row;
+static int8_t scroll_column, scroll_row;
+static uint8_t x_offset, y_offset;
 
 static void ui_draw_row_headers()
 {
@@ -57,37 +68,55 @@ static void ui_draw_column_headers()
     }
 }
 
-static void ui_draw_cell_value(uint8_t cell_column, uint8_t cell_row)
+static void ui_draw_cell_value(uint8_t cell_column, uint8_t cell_row, uint8_t x, uint8_t y)
 {
-    uint8_t cell_idx, active;
+    uint8_t cell_idx;
     const uint8_t* cell_value;
+    uint8_t chars_to_draw = (CANVAS_WIDTH_CHARS - x) < COLUMN_WIDTH ? (CANVAS_WIDTH_CHARS - x) : COLUMN_WIDTH;
 
     cell_value = c_get_cell_value(g_cell_ctx, cell_column, cell_row);
-    active = (active_cell_column == cell_column) && (active_cell_row == cell_row);
-    for (cell_idx = 0;cell_idx < COLUMN_WIDTH;++cell_idx)
-        s_put_symbol(cell_value[cell_idx], active ? INVERSE_COLOR : NORMAL_COLOR);    
+
+    for (cell_idx = 0;cell_idx < chars_to_draw;++cell_idx)
+        s_put_symbol(cell_value[cell_idx], NORMAL_COLOR);
+
+    if (chars_to_draw < COLUMN_WIDTH)
+    {
+        s_set_position(0, y, LAYER_CELLS);
+        for (;cell_idx < COLUMN_WIDTH;++cell_idx)
+            s_put_symbol(cell_value[cell_idx], NORMAL_COLOR);
+    }
 }
 
-static void ui_draw_cells()
+static void ui_draw_cells(uint8_t x, uint8_t y, uint8_t start_col, uint8_t end_col, uint8_t start_row, uint8_t end_row)
 {
-    uint8_t j, current_cell_row, current_cell_column, end_cell_column;
+    uint8_t current_cell_row, current_cell_column;
+    uint8_t current_x;
+    uint8_t current_y;
 
-    end_cell_column = ul_cell_column + NUMBER_CELL_COLUMNS - 1;
-
-    for (current_cell_row = ul_cell_row, j = 4;j < HEIGHT_CHARS;++j, ++current_cell_row)
+    for (current_cell_row = start_row;current_cell_row <= end_row;++current_cell_row)
     {
-        s_set_position(3, j, LAYER_CELLS);
-        for (current_cell_column = ul_cell_column;current_cell_column <= end_cell_column; ++current_cell_column)
+        current_x = x;
+        current_y = y + current_cell_row - start_row;
+
+        current_y = CLAMP(current_y, CANVAS_HEIGHT_CHARS);
+
+        s_set_position(current_x, current_y, LAYER_CELLS);
+        for (current_cell_column = start_col;current_cell_column <= end_col; ++current_cell_column)
         {
-            ui_draw_cell_value(current_cell_column, current_cell_row);
+            ui_draw_cell_value(current_cell_column, current_cell_row, current_x, current_y);
+            current_x += COLUMN_WIDTH;
+            if (current_x >= CANVAS_WIDTH_CHARS)
+            {
+                current_x -= CANVAS_WIDTH_CHARS;
+                s_set_position(current_x, current_y, LAYER_CELLS);
+            }
         }
     }
 }
 
-static void draw_single_cell(uint8_t cell_column, uint8_t cell_row)
+static void ui_update_scroll(void)
 {
-    s_set_position(3 + ((cell_column - ul_cell_column) * COLUMN_WIDTH), cell_row - ul_cell_row + 4, LAYER_CELLS);
-    ui_draw_cell_value(cell_column, cell_row);
+    s_scroll(scroll_column, scroll_row, LAYER_CELLS);
 }
 
 void ui_init(cell_ctx ctx)
@@ -98,13 +127,17 @@ void ui_init(cell_ctx ctx)
     g_cell_ctx = ctx;
 
     s_init();
-    s_clear(NORMAL_COLOR, LAYER_UI);
+    s_clear(TRANSPARENT_COLOR, LAYER_UI);
     s_clear(NORMAL_COLOR, LAYER_CELLS);
 
     active_cell_column = 0;
     active_cell_row = 0;
     ul_cell_column = 0;
     ul_cell_row = 0;
+    scroll_column = -3;
+    scroll_row = -4;
+    x_offset = 0;
+    y_offset = 0;
 
     s_set_position(0, 0, LAYER_UI);
     for (i = 0;i < WIDTH_CHARS;++i)
@@ -118,24 +151,37 @@ void ui_init(cell_ctx ctx)
         s_put_symbol(SYMBOL_SPACE, INVERSE_COLOR);
     }
 
+    s_set_position(0, 2, LAYER_UI);
+    for (i = 0;i < WIDTH_CHARS;++i)
+    {
+        s_put_symbol(SYMBOL_SPACE, NORMAL_COLOR);
+    }
+
     s_set_position(0, 3, LAYER_UI);
     for (i = 0;i < ((NUMBER_CELL_COLUMNS * COLUMN_WIDTH) + 3);++i)
     {
         s_put_symbol(SYMBOL_SPACE, INVERSE_COLOR);
     }
 
-    for (j = 4; j<HEIGHT_CHARS;++j)
+    for (j = 3; j<HEIGHT_CHARS;++j)
     {
         s_set_position(0, j, LAYER_UI);
         for (i = 0;i < 3;++i)
         {
             s_put_symbol(SYMBOL_SPACE, INVERSE_COLOR);
         }
+
+        s_set_position(3 + (NUMBER_CELL_COLUMNS * COLUMN_WIDTH), j, LAYER_UI);
+        for (i = 3 + (NUMBER_CELL_COLUMNS * COLUMN_WIDTH);i < WIDTH_CHARS;++i)
+        {
+            s_put_symbol(SYMBOL_SPACE, NORMAL_COLOR);
+        }
     }
 
     ui_draw_row_headers();
     ui_draw_column_headers();
-    ui_draw_cells();
+    ui_update_scroll();
+    ui_draw_cells(x_offset, y_offset, 0, NUMBER_CELL_COLUMNS, 0, NUMBER_CELL_ROWS);
 }
 
 static void adjust_active_cell(uint8_t new_active_cell_column, uint8_t new_active_cell_row)
@@ -148,8 +194,8 @@ static void adjust_active_cell(uint8_t new_active_cell_column, uint8_t new_activ
     active_cell_column = new_active_cell_column;
     active_cell_row = new_active_cell_row;
 
-    draw_single_cell(old_active_cell_column, old_active_cell_row);
-    draw_single_cell(active_cell_column, active_cell_row);
+    // draw_single_cell(old_active_cell_column, old_active_cell_row);
+    // draw_single_cell(active_cell_column, active_cell_row);
 }
 
 static void ensure_cell_in_view(uint8_t cell_column, uint8_t cell_row)
@@ -163,8 +209,8 @@ static void ensure_cell_in_view(uint8_t cell_column, uint8_t cell_row)
     if (cell_row < ul_cell_row)
         new_ul_cell_row = cell_row;
 
-    if (cell_row >= (ul_cell_row + (HEIGHT_CHARS - 4)))
-        new_ul_cell_row = (cell_row - (HEIGHT_CHARS - 4) + 1);
+    if (cell_row >= (ul_cell_row + NUMBER_CELL_ROWS))
+        new_ul_cell_row = (cell_row - NUMBER_CELL_ROWS + 1);
 
     if (cell_column >= (ul_cell_column + (NUMBER_CELL_COLUMNS)))
         new_ul_cell_column = cell_column - NUMBER_CELL_COLUMNS + 1;
@@ -175,36 +221,100 @@ static void ensure_cell_in_view(uint8_t cell_column, uint8_t cell_row)
         ul_cell_row = new_ul_cell_row;
         ui_draw_row_headers();
         ui_draw_column_headers();
-        ui_draw_cells();
+        // ui_draw_cells();
     }
 }
 
+#define COMPUTE_BB() { ul_x = CLAMP_SUB(x_offset, COLUMN_WIDTH, CANVAS_WIDTH_CHARS); ul_y = CLAMP_SUB(y_offset, 1, CANVAS_HEIGHT_CHARS); br_x = CLAMP_ADD(x_offset, (COLUMN_WIDTH * NUMBER_CELL_COLUMNS), CANVAS_WIDTH_CHARS); br_y = CLAMP_ADD(y_offset, NUMBER_CELL_ROWS, CANVAS_HEIGHT_CHARS);}
+
 void ui_kb(uint8_t key)
 {
-    uint8_t new_active_cell_column = active_cell_column;
-    uint8_t new_active_cell_row = active_cell_row;
-
+    uint8_t ul_x, ul_y, br_x, br_y;
     switch (key)
     {
         case CH_CURS_DOWN:
-            new_active_cell_row += 1;
+            ul_cell_row += 1;
+            scroll_row += 1;
+            y_offset = CLAMP_ADD(y_offset, 1, CANVAS_HEIGHT_CHARS);
+
+            COMPUTE_BB()
+
+            ui_draw_cells(
+                (ul_cell_column > 0) ? ul_x : x_offset,
+                br_y,
+                (ul_cell_column > 0) ? ul_cell_column - 1 : ul_cell_column,
+                ul_cell_column + NUMBER_CELL_COLUMNS,
+                ul_cell_row + NUMBER_CELL_ROWS,
+                ul_cell_row + NUMBER_CELL_ROWS
+            );
             break;
         case CH_CURS_UP:
-            if (new_active_cell_row > 0)
-                new_active_cell_row -= 1;
+            if (ul_cell_row > 0)
+            {
+                ul_cell_row -= 1;
+                scroll_row -= 1;
+                y_offset = CLAMP_SUB(y_offset, 1, CANVAS_HEIGHT_CHARS);
+
+                if (ul_cell_row > 0)
+                {
+                    COMPUTE_BB()
+
+                    ui_draw_cells(
+                        (ul_cell_column > 0) ? ul_x : x_offset,
+                        ul_y,
+                        (ul_cell_column > 0) ? ul_cell_column - 1 : ul_cell_column,
+                        ul_cell_column + NUMBER_CELL_COLUMNS,
+                        ul_cell_row - 1,
+                        ul_cell_row - 1
+                    );
+                }
+            }
             break;
         case CH_CURS_RIGHT:
-            new_active_cell_column += 1;
+            ul_cell_column += 1;
+            scroll_column += COLUMN_WIDTH;
+            x_offset = CLAMP_ADD(x_offset, COLUMN_WIDTH, CANVAS_WIDTH_CHARS);
+
+            COMPUTE_BB()
+
+            ui_draw_cells(
+                br_x,
+                (ul_cell_row > 0) ? ul_y : y_offset,
+                ul_cell_column + NUMBER_CELL_COLUMNS,
+                ul_cell_column + NUMBER_CELL_COLUMNS,
+                (ul_cell_row > 0) ? ul_cell_row - 1 : ul_cell_row,
+                ul_cell_row + NUMBER_CELL_ROWS
+            );
             break;
         case CH_CURS_LEFT:
-            if (new_active_cell_column > 0)
-                new_active_cell_column -= 1;
+            if (ul_cell_column > 0)
+            {
+                ul_cell_column -= 1;
+                scroll_column -= COLUMN_WIDTH;
+                x_offset = CLAMP_SUB(x_offset, COLUMN_WIDTH, CANVAS_WIDTH_CHARS);
+
+                if (ul_cell_column > 0)
+                {
+                    COMPUTE_BB()
+
+                    ui_draw_cells(
+                        ul_x,
+                        (ul_cell_row > 0) ? ul_y : y_offset,
+                        ul_cell_column - 1,
+                        ul_cell_column - 1,
+                        (ul_cell_row > 0) ? ul_cell_row - 1 : ul_cell_row,
+                        ul_cell_row + NUMBER_CELL_ROWS
+                    );
+                }
+            }
             break;
     }
 
-    if ((new_active_cell_column != active_cell_column) || (new_active_cell_row != active_cell_row))
-    {
-        ensure_cell_in_view(new_active_cell_column, new_active_cell_row);
-        adjust_active_cell(new_active_cell_column, new_active_cell_row);
-    }
+    ui_update_scroll();
+
+    // if ((new_active_cell_column != active_cell_column) || (new_active_cell_row != active_cell_row))
+    // {
+    //     ensure_cell_in_view(new_active_cell_column, new_active_cell_row);
+        // adjust_active_cell(new_active_cell_column, new_active_cell_row);
+    // }
 }
