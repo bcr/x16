e85142aae97cde326fb33d2154c2a242bc1bcbb7
Blake Ramsdell <blaker@gmail.com>
2023-11-02 14:22:17 -0700

    Add doxygen support

--- DerpiCalc/screen.h
+++ DerpiCalc/screen.h
@@ -1,11 +1,16 @@
+/// @file screen.h
+
 #ifndef SCREEN_H
 #define SCREEN_H
 
+/// @brief Width of the visible screen in characters.
 #define WIDTH_CHARS 80
+/// @brief Height of the visible screen in characters.
 #define HEIGHT_CHARS 60
 
 #define MAKECOLOR(FG, BG) ((FG) | (BG) << 4)
 
+/// @brief Initialize the screen routines.
 void s_init(void);
 void s_clear(uint8_t color, uint8_t layer);
 void s_set_position(uint8_t col, uint8_t row, uint8_t layer);
