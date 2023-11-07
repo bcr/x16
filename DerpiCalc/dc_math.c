21a1bfb2ed89a57e6b577202966c51bd2cb1e7f2
Blake Ramsdell <blaker@gmail.com>
2023-11-06 19:47:37 -0800

    Add divide

--- DerpiCalc/dc_math.c
+++ DerpiCalc/dc_math.c
@@ -8,6 +8,15 @@ static volatile const char* const fout_output = ((char*) 0x0100);
 static uint8_t temp_hi;
 static uint8_t temp_lo;
 
+static void get_fac(struct number_t* result)
+{
+    temp_hi = ((uint16_t) result) >> 8;
+    temp_lo = ((uint16_t) result) & 0x0ff;
+    asm("ldx %v", temp_lo);
+    asm("ldy %v", temp_hi);
+    asm("JSR $FE66"); // MOVMF -- Stores FAC IN Y/X (result), 5 bytes long
+}
+
 void m_int_to_number(int16_t i, struct number_t *result)
 {
     temp_hi = i >> 8;
@@ -16,11 +25,7 @@ void m_int_to_number(int16_t i, struct number_t *result)
     asm("lda %v", temp_hi);
     asm("JSR $FE03"); // GIVAYF -- puts A/Y into FAC
 
-    temp_hi = ((uint16_t) result) >> 8;
-    temp_lo = ((uint16_t) result) & 0x0ff;
-    asm("ldx %v", temp_lo);
-    asm("ldy %v", temp_hi);
-    asm("JSR $FE66"); // MOVMF -- Stores FAC IN Y/X (temp_number), 5 bytes long
+    get_fac(result);
 }
 
 const char* m_number_to_cstr(const struct number_t* n)
@@ -35,6 +40,24 @@ const char* m_number_to_cstr(const struct number_t* n)
     return (const char*) fout_output;
 }
 
+static void setup_fac_arg(const struct number_t* a, const struct number_t* b)
+{
+    temp_hi = ((uint16_t) a) >> 8;
+    temp_lo = ((uint16_t) a) & 0x0ff;
+    asm("lda %v", temp_lo);
+    asm("ldy %v", temp_hi);
+    asm("JSR $FE5D"); // ROMUPK -- move memory to ARG
+
+    temp_hi = ((uint16_t) b) >> 8;
+    temp_lo = ((uint16_t) b) & 0x0ff;
+    asm("lda %v", temp_lo);
+    asm("ldy %v", temp_hi);
+    asm("JSR $FE63"); // MOVFM -- move memory to FAC
+}
+
 void m_divide(const struct number_t* a, const struct number_t* b, struct number_t* result)
 {
+    setup_fac_arg(a, b);
+    asm("JSR $FE27"); // FDIVT -- FAC = ARG / FAC
+    get_fac(result);
 }
