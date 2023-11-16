53b25eb08522ddc051466878e3b2ef94b24c3661
Blake Ramsdell <blaker@gmail.com>
2023-11-16 00:20:52 -0800

    Add at min support

--- DerpiCalc/expr_at.c
+++ DerpiCalc/expr_at.c
@@ -265,6 +265,25 @@ static uint8_t handle_max(const uint8_t* buffer, uint8_t len, struct number_t* r
     return number_iter(buffer, len, &state, max_func);
 }
 
+static uint8_t min_func(void* state, const struct number_t* number)
+{
+    struct max_state* local_state = state;
+
+    if ((local_state->first) || (m_compare(number, local_state->result) < 0))
+        memcpy(local_state->result, number, sizeof(struct number_t));
+    local_state->first = 0;
+
+    return EVALUATE_OK;
+}
+
+static uint8_t handle_min(const uint8_t* buffer, uint8_t len, struct number_t* result)
+{
+    struct max_state state;
+    state.first = 1;
+    state.result = result;
+    return number_iter(buffer, len, &state, min_func);
+}
+
 struct at_func
 {
     const char* name;
@@ -283,6 +302,7 @@ static const struct at_func nonzero_len_at_funcs[] = {
     { "ABS", handle_abs },
     { "SUM", handle_sum },
     { "MAX", handle_max },
+    { "MIN", handle_min },
 
     { NULL, NULL }
     };
